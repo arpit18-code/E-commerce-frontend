0ff8a424359a65410714fe976411ac38d7baa1d4
arpit18-code <arpitjain18105@gmail.com>
2025-09-25 02:30:55 +0530

    added basename to browserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/E-commerce-frontend">
         <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/home" element={<Home />} />
